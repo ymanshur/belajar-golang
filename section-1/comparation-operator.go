c6679277ccea7673baa7e9ef8b52b08e79b19c53
Yusuf Manshur <ymanshur@gmail.com>
2022-10-20 14:18:34 +0700

    feat(section-1): add comparation-operator.go

---
+++ section-1/comparation-operator.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "Eko"
+	var name2 = "Eko"
+
+	var result bool = name1 == name2
+	fmt.Println(result)
+
+	var value1 = 100
+	var value2 = 200
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+}
