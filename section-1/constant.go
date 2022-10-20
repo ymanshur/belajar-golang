af9464b5042dca792544705d398094b7b298845b
Yusuf Manshur <ymanshur@gmail.com>
2022-10-20 13:58:56 +0700

    feat(section-1): add constant.go

---
+++ section-1/constant.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// const firstName string = "Eko"
+	// const lastName = "Khannedy"
+	// const value = 1000
+
+	const (
+		firstName string = "Eko"
+		lastName         = "Khannedy"
+		value            = 1000
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+}
