92373b928f2deef748eaacf5f5c1bee135e2dcf8
Yusuf Manshur <ymanshur@gmail.com>
2022-10-25 09:26:29 +0700

    feat(section-1): add interface.go

---
+++ section-1/interface.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type HasName interface {
+	GetName() string
+}
+
+func sayHello(hasName HasName) {
+	fmt.Println("Hello", hasName.GetName())
+}
+
+type Person struct {
+	Name string
+}
+
+func (person Person) GetName() string {
+	return person.Name
+}
+
+type Animal struct {
+	Name string
+}
+
+func (animal Animal) GetName() string {
+	return animal.Name
+}
+
+func main() {
+	var yusuf Person
+	yusuf.Name = "Yusuf"
+
+	sayHello(yusuf)
+
+	var cat Animal
+	cat.Name = "Abu"
+
+	sayHello(cat)
+
+}
