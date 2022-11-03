8b2bc36abb024eca276d2b05a03b48abcfe2f84e
Yusuf Manshur <ymanshur@gmail.com>
2022-11-03 12:23:23 +0700

    feat(import): try access helper func and method

---
+++ section-1/import.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"belajar-golang-dasar/section-1/helper"
+	"fmt"
+)
+
+func main() {
+	helper.SayHello("Yusuf")
+	// helper.sayGoodBye("Yusuf")
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version)
+}
