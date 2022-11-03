a7fcef996a7353103ed6496ba2b48dfbe2b2d751
Yusuf Manshur <ymanshur@gmail.com>
2022-11-03 22:11:20 +0700

    feat(hello_world_test): using testify package

--- section-3/belajar-golang-unit-test/helper/hello_world_test.go
+++ section-3/belajar-golang-unit-test/helper/hello_world_test.go
@@ -3,7 +3,24 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Yusuf")
+	require.Equal(t, "Hello Yusuf", result, "Result must be 'Hello Yusuf'")
+	fmt.Println("TestHelloWorldRequire Done")
+
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Yusuf")
+	assert.Equal(t, "Hello Yusuf", result, "Result must be 'Hello Yusuf'")
+	fmt.Println("TestHelloWorldAssert Done")
+
+}
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Yusuf")
