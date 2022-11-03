aaed018dc198bf82b1cc736353acbd8e179a7f50
Yusuf Manshur <ymanshur@gmail.com>
2022-11-03 19:15:55 +0700

    feat(belajar-golang-unit-test): thwart HelloWorld unit test

--- section-3/belajar-golang-unit-test/helper/hello_world_test.go
+++ section-3/belajar-golang-unit-test/helper/hello_world_test.go
@@ -1,19 +1,30 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Yusuf")
 	if result != "Hello Yusuf" {
 		// unit test failed
-		panic("Result is not 'Hello Yusuf'")
+		// panic("Result is not 'Hello Yusuf'")
+		// t.Fail()
+		t.Error("Result must be 'Hello Yusuf'")
 	}
+
+	fmt.Println("TestHelloWorld Done")
 }
 
 func TestHelloWorldManshur(t *testing.T) {
 	result := HelloWorld("Manshur")
 	if result != "Hello Manshur" {
 		// unit test failed
-		panic("Result is not 'Hello Manshur'")
+		// panic("Result is not 'Hello Manshur'")
+		// t.FailNow()
+		t.Fatal("Result must be 'Hello Manshur'")
 	}
+
+	fmt.Println("TestHelloWorldManshur Done")
 }
