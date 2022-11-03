f29845fad7ab465b38b708bcd950a1885bb244e7
Yusuf Manshur <ymanshur@gmail.com>
2022-11-03 22:14:30 +0700

    feat(hello_world_test): implement skip test

--- section-3/belajar-golang-unit-test/helper/hello_world_test.go
+++ section-3/belajar-golang-unit-test/helper/hello_world_test.go
@@ -2,11 +2,21 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		t.Skip("Can not run on Mac OS")
+	}
+
+	result := HelloWorld("Yusuf")
+	require.Equal(t, "Hello Yusuf", result, "Result must be 'Hello Yusuf'")
+}
 
 func TestHelloWorldRequire(t *testing.T) {
 	result := HelloWorld("Yusuf")
