8718554074093ec3bb9a506c6e209c1110b11402
Yusuf Manshur <ymanshur@gmail.com>
2022-11-04 14:44:08 +0700

    feat(channel_test): imlement channel as parameter

--- section-4/belajar-golang-goroutine/channel_test.go
+++ section-4/belajar-golang-goroutine/channel_test.go
@@ -29,3 +29,21 @@ func TestCreateChannel(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func GiveMeResponse(channel chan string) {
+	channel <- "Yusuf Manshur"
+
+	time.Sleep(2 * time.Second)
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
