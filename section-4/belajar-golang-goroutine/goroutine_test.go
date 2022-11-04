114f97e5ddb0135b4d8846750af96613cf16a0d3
Yusuf Manshur <ymanshur@gmail.com>
2022-11-04 14:04:48 +0700

    feat(goroutine_test): implemen goroutine

--- section-4/belajar-golang-goroutine/goroutine_test.go
+++ section-4/belajar-golang-goroutine/goroutine_test.go
@@ -16,3 +16,18 @@ func TestCreateGoroutine(t *testing.T) {
 
 	time.Sleep(1 * time.Second)
 }
+
+func DisplayNumber(number int) {
+	fmt.Println("Display", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		// TestManyGoroutine (31.39s)
+		// DisplayNumber(i)
+		// TestManyGoroutine (5.92s)
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(5 * time.Second)
+}
