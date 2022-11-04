7e63bd20476bb32e3923c2fb419756401677177c
Yusuf Manshur <ymanshur@gmail.com>
2022-11-04 14:50:14 +0700

    feat(channel_test): implement only in-out channel as param

--- section-4/belajar-golang-goroutine/channel_test.go
+++ section-4/belajar-golang-goroutine/channel_test.go
@@ -47,3 +47,27 @@ func TestChannelAsParameter(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func OnlyIn(channel chan<- string) {
+	channel <- "Yusuf Manshur"
+	// invalid operation
+	// data := <-channel
+	time.Sleep(2 * time.Second)
+}
+
+func OnlyOut(channel <-chan string) {
+	// invalid operation
+	// channel <- "Yusuf Manshur"
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
