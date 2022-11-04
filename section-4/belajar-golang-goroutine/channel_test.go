adee4000c75caeebebfec9eae9292f9da6a6d7ff
Yusuf Manshur <ymanshur@gmail.com>
2022-11-04 15:34:49 +0700

    feat(channel_test): implement select channel

--- section-4/belajar-golang-goroutine/channel_test.go
+++ section-4/belajar-golang-goroutine/channel_test.go
@@ -112,3 +112,43 @@ func TestRangeChannel(t *testing.T) {
 
 	fmt.Println("Done")
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	// select {
+	// case data := <-channel1:
+	// 	fmt.Println("Data dari Channel 1", data)
+	// case data := <-channel2:
+	// 	fmt.Println("Data dari Channel 2", data)
+	// }
+
+	// select {
+	// case data := <-channel1:
+	// 	fmt.Println("Data dari Channel 1", data)
+	// case data := <-channel2:
+	// 	fmt.Println("Data dari Channel 2", data)
+	// }
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
