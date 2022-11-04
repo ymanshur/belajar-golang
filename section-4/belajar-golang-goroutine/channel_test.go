3be2a6bca2614f7b6db5c5486eae7ee2ab63b388
Yusuf Manshur <ymanshur@gmail.com>
2022-11-04 15:36:50 +0700

    feat(channel_test): add default select

--- section-4/belajar-golang-goroutine/channel_test.go
+++ section-4/belajar-golang-goroutine/channel_test.go
@@ -152,3 +152,31 @@ func TestSelectChannel(t *testing.T) {
 		}
 	}
 }
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		default:
+			fmt.Println("Waiting...")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
