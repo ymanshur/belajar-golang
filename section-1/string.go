b46f02f83850952db081ac8f266afa5a71cc9a4c
Yusuf Manshur <yusuf.manshur@privyid.tech>
2023-01-31 12:29:31 +0700

    feat(string): add len() comment

--- section-1/string.go
+++ section-1/string.go
@@ -7,7 +7,7 @@ func main() {
 	fmt.Println("Eko Kurniawan")
 	fmt.Println("Eko Kurniawan Khannedy")
 
-	fmt.Println(len("Eko"))
+	fmt.Println(len("Eko"))                  // return []byte of 'Eko'
 	fmt.Println("Eko Kurniawan"[0])          // return byte character of 'E'
 	fmt.Println("Eko Kurniawan Khannedy"[1]) // return byte character of 'k'
 }
