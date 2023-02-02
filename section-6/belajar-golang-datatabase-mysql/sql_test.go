53478b20c5de7661f78c6a2dfb58a78a278ee466
Yusuf Manshur <yusuf.manshur@privyid.tech>
2023-02-02 12:46:53 +0700

    feat(mysql_test): test mysql complex query func

--- section-6/belajar-golang-datatabase-mysql/sql_test.go
+++ section-6/belajar-golang-datatabase-mysql/sql_test.go
@@ -2,8 +2,10 @@ package belajar_golang_datatabase
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -44,5 +46,50 @@ func TestQuerySql(t *testing.T) {
 
 		fmt.Println("Id:", id)
 		fmt.Println("Name:", name)
+	}
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name, email, balance, rating, birth_date, married, created_at FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString
+		var balance int32
+		var rating sql.NullInt32
+		var birthDate sql.NullTime
+		var createdAt time.Time
+		var married bool
+
+		err := rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createdAt) // sesuai urutan kolom di table
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("========================")
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+		if email.Valid {
+			fmt.Println("Email:", email.String)
+		}
+		fmt.Println("Balance:", balance)
+		if rating.Valid {
+			fmt.Println("Rating:", rating.Int32)
+		}
+		if birthDate.Valid {
+			fmt.Println("Birth Date:", birthDate.Time)
+		}
+		fmt.Println("Married?:", married)
+		fmt.Println("Created At:", createdAt)
 	}
 }
