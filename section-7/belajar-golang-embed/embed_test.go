71f21ce7fa4c57205dd16174e2ac09de4ae3e19a
Yusuf Manshur <yusuf.manshur@privyid.tech>
2023-02-02 17:43:32 +0700

    feat(section-7): embed file to string

---
+++ section-7/belajar-golang-embed/embed_test.go
@@ -0,0 +1,14 @@
+package belajar_golang_embed
+
+import (
+	_ "embed"
+	"fmt"
+	"testing"
+)
+
+//go:embed version.txt
+var version string
+
+func TestString(t *testing.T) {
+	fmt.Println(version)
+}
