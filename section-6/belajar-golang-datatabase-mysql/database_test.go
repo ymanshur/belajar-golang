20efc33a0868c6bfdad2caefbf7120a143c63f08
Yusuf Manshur <yusuf.manshur@privyid.tech>
2023-02-02 12:46:24 +0700

    feat(TestOpenConnection): add comment

--- section-6/belajar-golang-datatabase-mysql/database_test.go
+++ section-6/belajar-golang-datatabase-mysql/database_test.go
@@ -8,6 +8,8 @@ import (
 )
 
 func TestOpenConnection(t *testing.T) {
+	// create database pool (pool of connection)
+	// minimum mysql connection is ~150
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/belajar_golang_database")
 	if err != nil {
 		panic(err)
