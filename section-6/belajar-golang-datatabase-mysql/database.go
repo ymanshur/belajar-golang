71b69d74efbed9492305916e306de703620503ce
Yusuf Manshur <yusuf.manshur@privyid.tech>
2023-02-02 12:34:53 +0700

    feat(GetConnection): enable parse date

--- section-6/belajar-golang-datatabase-mysql/database.go
+++ section-6/belajar-golang-datatabase-mysql/database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func GetConnection() *sql.DB {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/belajar_golang_database")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/belajar_golang_database?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
