a54b886825b22f9583dbf341e8ab5df53e25228a
Yusuf Manshur <yusuf.manshur@privyid.tech>
2023-02-02 17:47:36 +0700

    feat(belajar-golang-embed): test embed image to bytes

--- section-7/belajar-golang-embed/embed_test.go
+++ section-7/belajar-golang-embed/embed_test.go
@@ -3,6 +3,8 @@ package belajar_golang_embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
@@ -11,4 +13,14 @@ var version string
 
 func TestString(t *testing.T) {
 	fmt.Println(version)
+}
+
+//go:embed logo.png
+var logo []byte
+
+func TestByte(t *testing.T) {
+	err := ioutil.WriteFile("logo_new.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
