9eb874f8cbd4d5b0be317366dcabae79c089ab5e
Yusuf Manshur <yusuf.manshur@privyid.tech>
2023-02-02 13:42:55 +0700

    feat(mysql_test): test mysql prepare statement

--- section-6/belajar-golang-datatabase-mysql/sql_test.go
+++ section-6/belajar-golang-datatabase-mysql/sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -180,4 +181,35 @@ func TestAutoIncrement(t *testing.T) {
 	}
 
 	fmt.Println("Success add user", email, "with ID", insertId)
+}
+
+func TestPrepareStatement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := "INSERT INTO comments(email, comment) VALUES (?, ?)"
+	statement, err := db.PrepareContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+	defer statement.Close()
+
+	for i := 0; i < 10; i++ {
+		email := "eko" + strconv.Itoa(i+1) + "@gmail.com"
+		comment := "comment " + strconv.Itoa(i+1)
+
+		result, err := statement.ExecContext(ctx, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Comment Id ", id)
+	}
 }
